256d5843f2cd954bc8f23411fb84962aa482000d
Raoqz <sayaralex@gmail.com>
2024-03-21 23:37:14 +0100

    Defining and applying the column's bonuses

--- player.py
+++ player.py
@@ -1,8 +1,11 @@
 from database import Database
 
+
+# yes
 class Player:
     def __init__(self, board, id76):
         self.gold = 0
         self.boat_destroyed = False
         self.board = board
         self.id76 = id76
+
