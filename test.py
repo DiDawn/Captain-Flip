6fdc2e0790164877c3b4768222d43c29eb44b9a3
didawn <pitoisnoah@gmail.com>
2024-03-28 16:30:21 +0100

    add sprite class for tiles

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pygame as pygame
-from surface_board import Tile
+from tile_sprite import Tile
 
 
 pygame.init()
@@ -7,9 +7,23 @@ screen = pygame.display.set_mode((640, 480))
 
 group = pygame.sprite.Group()
 
-tile = Tile((100, 100), ('assets/tiles/1.png', 'assets/tiles/2.png'))
+tile = Tile((100, 100), ('assets/tiles/cabin_boy.jpg', 'assets/tiles/monkey.jpg'))
 tile.set_position((100, 100))
 group.add(tile)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if tile.rect.collidepoint(event.pos):
+                tile.flip()
+
+    group.update()
+    screen.fill((0, 0, 0))
+    group.draw(screen)
+    pygame.display.flip()
 
 
 if __name__ == '__main__':
